a403022da1dce599455b0101076f8ed43813949b
Daria Matveeva <90855097+dashadashad4sha@users.noreply.github.com>
2022-10-26 18:27:56 +0500

    Create sanctions_parsing.py

---
+++ parsers/sanctions_parsing.py
@@ -0,0 +1,47 @@
+# Это парсер для санкций. Для запуска нужно использовать функцию sanctions_parsing,
+# и все данные сохранятся в нужной директории, разделенные по странам
+
+import requests
+from bs4 import BeautifulSoup as BS
+import pandas as pd
+
+
+def sanctions_parsing():
+  r = requests.get(f'https://wto.ru/uchastnikam-ved-sanktsii/')
+  html = BS(r.content, 'html.parser')
+
+  table1 = html.find("table")
+
+  headers = ['country_name', 'what_i_need', 'other']
+
+  mydata = pd.DataFrame(columns=headers)
+
+  for j in table1.find_all("tr")[:]:
+      try:
+          row_data = j.find_all("td")
+          row = [i for i in row_data]
+          length = len(mydata)
+          mydata.loc[length] = row
+      except:
+          continue
+
+  mydata['country_name'][0].text.replace("\n", "").replace("\t", "").replace("\xa0", "").strip()
+
+  list_of_hrefs_html = mydata["what_i_need"]
+
+  dict_of_hrefs = {}
+
+  l = len(list_of_hrefs_html)
+  for i in range(l):
+    s = list_of_hrefs_html[i].p
+    a = s.find("a").get("href")
+    name = mydata['country_name'][i].text.replace("\n", "").replace("\t", "").replace("\xa0", "").strip()
+    dict_of_hrefs[name] = a
+
+  for name in dict_of_hrefs:
+    href = dict_of_hrefs[name]
+    res = requests.get(f'https://wto.ru{href}').content
+    with open(f"{name}_санкции.xlsx", 'wb') as file:
+      file.write(res)
+  return "Done"
+
