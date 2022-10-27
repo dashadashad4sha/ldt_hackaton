4c94b8109f3f4fe4fdbb2a8df873f58770749605
a_polumestny <anpolumestny@gmail.com>
2022-10-27 14:27:08 +0300

    add server ip to allowed hosts

--- backend/recommendation_service/settings.py
+++ backend/recommendation_service/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['188.72.108.79']
 
 
 # Application definition
