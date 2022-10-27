cb4e73900ec996feb36d552ecfe98c622448ba33
a_polumestny <anpolumestny@gmail.com>
2022-10-27 10:49:53 +0300

    add readmi.md, admin site

---
+++ backend/READE.md
@@ -0,0 +1,17 @@
+## start project
+1) create and activate virtual environment
+```
+python3 -m venv venv
+source ./venv/bin/activate
+```
+2) install requirements
+```pip install -r requirements.txt```
+3) move to project dir
+```
+cd backend
+python manage.py runserver
+```
+4)[admin site](http://127.0.0.1:8000/admin)
+
+username=admin
+password=admin
--- backend/customs/admin.py
+++ backend/customs/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from customs.models import Unit, Region, Country, CustomData, CustomTnvedCode, FederalDistrict
 # Register your models here.
+
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ['unit_id', 'unit_code', 'unit_name']
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['region_id', 'region_code', 'region_name', 'federal_district']
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ['country_id', 'country_name', 'country_block']
+
+
+@admin.register(FederalDistrict)
+class FederalDistrictAdmin(admin.ModelAdmin):
+    list_display = ['federal_district_id', 'federal_district_code', 'federal_district_name']
+
+
+@admin.register(CustomTnvedCode)
+class CustomTnvedCodeAdmin(admin.ModelAdmin):
+    list_display = ['tnved_id', 'tnved_code', 'tnved_name', 'parent_tnved']
