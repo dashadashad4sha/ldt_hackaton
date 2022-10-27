e2080fdf4a2b3a63c4a0a2f7cc7aaf504b1c9441
Daria Matveeva <90855097+dashadashad4sha@users.noreply.github.com>
2022-10-27 15:09:59 +0500

    исправила запись файлов в sanction_parsing.py

--- parsers/sanctions_parsing.py
+++ parsers/sanctions_parsing.py
@@ -1,47 +1,53 @@
 # Это парсер для санкций. Для запуска нужно использовать функцию sanctions_parsing,
 # и все данные сохранятся в нужной директории, разделенные по странам
 
+import os
 import requests
 from bs4 import BeautifulSoup as BS
 import pandas as pd
 
 
 def sanctions_parsing():
-  r = requests.get(f'https://wto.ru/uchastnikam-ved-sanktsii/')
-  html = BS(r.content, 'html.parser')
+    r = requests.get(f'https://wto.ru/uchastnikam-ved-sanktsii/')
+    html = BS(r.content, 'html.parser')
 
-  table1 = html.find("table")
+    table1 = html.find("table")
 
-  headers = ['country_name', 'what_i_need', 'other']
+    headers = ['country_name', 'what_i_need', 'other']
 
-  mydata = pd.DataFrame(columns=headers)
+    mydata = pd.DataFrame(columns=headers)
 
-  for j in table1.find_all("tr")[:]:
-      try:
-          row_data = j.find_all("td")
-          row = [i for i in row_data]
-          length = len(mydata)
-          mydata.loc[length] = row
-      except:
-          continue
+    for j in table1.find_all("tr")[:]:
+        try:
+            row_data = j.find_all("td")
+            row = [i for i in row_data]
+            length = len(mydata)
+            mydata.loc[length] = row
+        except:
+            continue
 
-  mydata['country_name'][0].text.replace("\n", "").replace("\t", "").replace("\xa0", "").strip()
+    mydata['country_name'][0].text.replace("\n", "").replace("\t", "").replace("\xa0", "").strip()
 
-  list_of_hrefs_html = mydata["what_i_need"]
+    list_of_hrefs_html = mydata["what_i_need"]
 
-  dict_of_hrefs = {}
+    dict_of_hrefs = {}
 
-  l = len(list_of_hrefs_html)
-  for i in range(l):
-    s = list_of_hrefs_html[i].p
-    a = s.find("a").get("href")
-    name = mydata['country_name'][i].text.replace("\n", "").replace("\t", "").replace("\xa0", "").strip()
-    dict_of_hrefs[name] = a
+    l = len(list_of_hrefs_html)
+    for i in range(l):
+        s = list_of_hrefs_html[i].p
+        a = s.find("a").get("href")
+        name = mydata['country_name'][i].text.replace("\n", "").replace("\t", "").replace("\xa0", "").strip()
+        dict_of_hrefs[name] = a
 
-  for name in dict_of_hrefs:
-    href = dict_of_hrefs[name]
-    res = requests.get(f'https://wto.ru{href}').content
-    # кто-нибудь, исправьте эту строчку
-    with open(f"D:/ldt_hackaton/preprocessing/sanctions_data/{name}_санкции.xlsx", 'wb') as file:
-      file.write(res)
-  return "Done"
+    for name in dict_of_hrefs:
+        href = dict_of_hrefs[name]
+        res = requests.get(f'https://wto.ru{href}').content
+
+
+
+        os.chdir(r'/ldt_hackaton-backend/preprocessing/sanctions_data')
+        with open(f"{name}_санкции.xlsx", 'wb') as file:
+            file.write(res)
+
+
+    return "Done"
