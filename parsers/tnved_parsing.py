06cec0f9c9aa506a2cdd003a015a0415b98a2e96
Daria Matveeva <90855097+dashadashad4sha@users.noreply.github.com>
2022-10-27 15:13:26 +0500

    Update tnved_parsing.py

--- parsers/tnved_parsing.py
+++ parsers/tnved_parsing.py
@@ -8,10 +8,9 @@ import pandas as pd
 def tnved_parsing():
 
     res = requests.get(f'https://economy.gov.ru/material/file/08d71eb4ee2c8160078d51700e7454bb/%D0%A2%D0%9D%D0%92%D0%AD%D0%94_%D0%9E%D0%9A%D0%9F%D0%942.xlsx').content
-    # k = os.chdir("preprocessing")
-    # with open(f"{k}/preprocessing/sanctions_data/{name}_санкции.xlsx", 'wb') as file:
-    with open(f"D:/idt_hackaton_project/preprocessing/tnved_data/Коды_тнвэд_окпдэ.xlsx", 'wb') as file:
+    os.chdir(r'/ldt_hackaton-backend/preprocessing/tnved_data')
+    with open(f"Коды_тнвэд_окпдэ.xlsx", 'wb') as file:
         file.write(res)
 
 
-tnved_parsing()
+# tnved_parsing()
