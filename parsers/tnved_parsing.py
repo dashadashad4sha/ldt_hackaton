b7106d03615748dc0fa44cbb94f984eb7a44d71b
Daria Matveeva <90855097+dashadashad4sha@users.noreply.github.com>
2022-10-28 12:05:06 +0500

    Update tnved_parsing.py

--- parsers/tnved_parsing.py
+++ parsers/tnved_parsing.py
@@ -1,13 +1,16 @@
 import os
-
 import requests
 from bs4 import BeautifulSoup as BS
 import pandas as pd
 
 
 def tnved_parsing():
+    """ получаем файл с расшифровкой кодов ТНВЭД и ОКПДЭ  """
 
     res = requests.get(f'https://economy.gov.ru/material/file/08d71eb4ee2c8160078d51700e7454bb/%D0%A2%D0%9D%D0%92%D0%AD%D0%94_%D0%9E%D0%9A%D0%9F%D0%942.xlsx').content
     os.chdir(r'/ldt_hackaton-backend/preprocessing/tnved_data')
     with open(f"Коды_тнвэд_окпдэ.xlsx", 'wb') as file:
         file.write(res)
+
+if __name__ == "__main__":
+    tnved_parsing()
