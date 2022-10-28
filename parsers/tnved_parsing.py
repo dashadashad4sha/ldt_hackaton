604706dc5e22663242d12a73e6171186bc1973a2
Daria Matveeva <90855097+dashadashad4sha@users.noreply.github.com>
2022-10-28 20:11:31 +0500

    Update tnved_parsing.py

--- parsers/tnved_parsing.py
+++ parsers/tnved_parsing.py
@@ -4,13 +4,15 @@ import requests
 from bs4 import BeautifulSoup as BS
 import pandas as pd
 
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 def tnved_parsing():
 
     res = requests.get(f'https://economy.gov.ru/material/file/08d71eb4ee2c8160078d51700e7454bb/%D0%A2%D0%9D%D0%92%D0%AD%D0%94_%D0%9E%D0%9A%D0%9F%D0%942.xlsx').content
-    os.chdir(r'/ldt_hackaton-backend/preprocessing/tnved_data')
-    with open(f"Коды_тнвэд_окпдэ.xlsx", 'wb') as file:
-        file.write(res)
+#     os.chdir(r'/ldt_hackaton-backend/preprocessing/tnved_data')
+#     with open(f"Коды_тнвэд_окпдэ.xlsx", 'wb') as file:
+#         file.write(res)
+      with open(f"{BASE_DIR}/Коды_тнвэд_окпдэ.xlsx", 'wb') as file:
+          file.write(res)
 
 
 # tnved_parsing()
