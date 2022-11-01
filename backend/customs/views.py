5a504e3ef53cf2286fa93d3a810f2189aa5b63fd
Daria Matveeva <90855097+dashadashad4sha@users.noreply.github.com>
2022-11-01 19:59:24 +0500

    Update views.py

--- backend/customs/views.py
+++ backend/customs/views.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets, mixins
 
-from customs.models import Unit, Region, Country, CustomTnvedCode, FederalDistrict, CustomData
+from customs.models import Unit, Region, Country, CustomTnvedCode, FederalDistrict, CustomData, Recommendation, Sanction
 from customs.serializers import UnitSerializer, RegionSerializer, CountrySerializer, FederalDistrictSerializer, \
-    TnvedCodeSerializer, CustomDataSerializer
+    TnvedCodeSerializer, CustomDataSerializer, SanctionSerializer, RecommendationSerializer
 
 
 class UnitView(viewsets.GenericViewSet,
@@ -51,3 +51,20 @@ class CustomDataView(viewsets.GenericViewSet,
                      ):
     queryset = CustomData.objects.all()
     serializer_class = CustomDataSerializer
+
+
+class SanctionView(viewsets.GenericViewSet,
+                   mixins.ListModelMixin,
+                   mixins.RetrieveModelMixin
+                   ):
+    queryset = Sanction.objects.all()
+    serializer_class = SanctionSerializer
+
+
+class RecommendationView(viewsets.GenericViewSet,
+                         mixins.ListModelMixin,
+                         mixins.RetrieveModelMixin
+                         ):
+    queryset = Recommendation.objects.all()
+    serializer_class = RecommendationSerializer
+
