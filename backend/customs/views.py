98ee6715c91ff952e840c7e6bbbd7a36c7e48f16
a_polumestny <anpolumestny@gmail.com>
2022-11-02 14:06:02 +0300

    add doc for top recommendatios

--- backend/customs/views.py
+++ backend/customs/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, mixins
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import viewsets, mixins, status
 from rest_framework.decorators import action
 from django.db.models import Sum
 from rest_framework.response import Response
@@ -6,6 +7,11 @@ from rest_framework.response import Response
 from customs.models import Unit, Region, Country, CustomTnvedCode, FederalDistrict, CustomData, Recommendation, Sanction
 from customs.serializers import UnitSerializer, RegionSerializer, CountrySerializer, FederalDistrictSerializer, \
     TnvedCodeSerializer, CustomDataSerializer, SanctionSerializer, RecommendationSerializer, TopRecommendationSerializer
+
+
+doc_get_top_recommendation_resp = {
+    status.HTTP_200_OK: TopRecommendationSerializer(many=True)
+}
 
 
 class UnitView(viewsets.GenericViewSet,
@@ -75,6 +81,8 @@ class RecommendationView(viewsets.GenericViewSet,
     queryset = Recommendation.objects.all()
     serializer_class = RecommendationSerializer
 
+    @swagger_auto_schema(operation_id='config',
+                         responses=doc_get_top_recommendation_resp)
     @action(methods=['GET'], detail=False, url_path='top')
     def top_recommendation(self, request, *args, **kargs):
         instance = Recommendation.objects.prefetch_related('region').filter(region__region_name='Москва').values(
