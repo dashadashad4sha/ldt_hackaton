a965560a71af72f4d44cf32f71c609707405c738
a_polumestny <anpolumestny@gmail.com>
2022-11-03 00:34:35 +0300

    fix raw sql

--- backend/customs/models.py
+++ backend/customs/models.py
@@ -54,7 +54,7 @@ class Region(models.Model):
 
 class CustomTnvedCode(models.Model):
     tnved_id = models.IntegerField(unique=True, blank=False)
-    tnved_code = models.BigIntegerField()
+    tnved_code = models.CharField()
     tnved_name = models.CharField(max_length=400)
     tnved_fee = models.CharField(max_length=200, null=True)
     create_time = models.DateTimeField(auto_now_add=True)
