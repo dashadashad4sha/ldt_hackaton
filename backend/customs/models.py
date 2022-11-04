3e58b64332cfe43642a34de190e7b0737a6e2e1c
a_polumestny <anpolumestny@gmail.com>
2022-11-05 00:38:12 +0300

    add analitic

--- backend/customs/models.py
+++ backend/customs/models.py
@@ -78,7 +78,7 @@ class CustomTnvedCode(models.Model):
     # ToDO rewrite to Django ORM
     def customs_partner_by_tnved(self, period, code_filter, region_filter):
         with connection.cursor() as cursor:
-            cursor.execute(raw_sql.customs_partner_by_tnved.format(period, code_filter, region_filter))
+            cursor.execute(raw_sql.customs_partner_by_tnved.format(period, code_filter, region_filter, period, code_filter, region_filter))
             columns = [col[0] for col in cursor.description]
             resp = [
                 dict(zip(columns, row))
